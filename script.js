875d37db4c86f20ede4898d3d50bf31a85be1b1e
JOHIRRIHAN <jr7718301@gmail.com>
2024-01-30 12:19:56 +0600

    add friend btn complete

--- script.js
+++ script.js
@@ -13,15 +13,35 @@
 // })
 
 
-var heading = document.querySelector("h5")
-var addFriends = document.querySelector("#add")
-var remove = document.querySelector("#remove")
+// var heading = document.querySelector("h5")
+// var addFriends = document.querySelector("#add")
+// var check = 0
+// addFriends.addEventListener("click",function(){
+//     if(check == 0){
+//     heading.innerHTML = "friends"
+//     heading.style.color= "green"
+//     check = 1
+//     }else{
+//      heading.innerHTML = "Stranger"
+//     heading.style.color= "rgb(44, 83, 95)"
+//     check = 0
+//     }
+// })
 
-addFriends.addEventListener("click",function(){
-    heading.innerHTML = "friends"
-    heading.style.color= "green"
-})
-remove.addEventListener("click",function(){
-    heading.innerHTML = "Stranger"
-    heading.style.color= "rgb(44, 83, 95)"
+
+var heading = document.querySelector("h5")
+var btn = document.querySelector("#add")
+var check = 0
+btn.addEventListener("click",function(){
+    if(check ==0){
+        heading.innerHTML= "Friends"
+        heading.style.color = "red"
+        btn.innerHTML = "Remove Friend"
+        check =1
+    }else{
+        heading.innerHTML= "Stranger"
+        heading.style.color = "rgb(44, 83, 95)"
+        btn.innerHTML = "Add Friend"
+        check= 0
+    }
 })
